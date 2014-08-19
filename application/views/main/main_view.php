8d680d6447b812c755ba561e818df1dab921207f
Ahmad Sodiqin <ahmad.shodiqin88@gmail.com>
2014-08-19 11:22:50 +0700

    hard desain

--- application/views/main/main_view.php
+++ application/views/main/main_view.php
@@ -1,19 +1,127 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
-<title>Portal News</title>
-</head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
-	    <body>
-	    <p>This page uses frames. The current browser you are using does not support frames.</p>
-	    <?php
+	<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Portal News</title>
 	
-		?>
-	    </body>
-    </noframes>
-</frameset>
+	<!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Bootstrap theme -->
+    <link href="<?php echo base_url();?>assets/css/bootstrap-theme.min.css" rel="stylesheet">
+	
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url();?>assets/css/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="<?php echo base_url();?>assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="<?php echo base_url();?>assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			
+			<div class="header">
+		        <ul class="nav nav-pills pull-right">
+		          <li class="active"><a href="#">Home</a></li>
+		          <li><a href="#">About</a></li>
+		          <li><a href="#">Contact</a></li>
+		        </ul>
+		    	<h3 class="text-muted">Logo</h3>
+			</div>
+			
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				  </ol>
+				
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner">
+				    <div class="item active">
+				      <img src="<?php echo base_url();?>assets/img/Desert.jpg" alt="...">
+				      <div class="carousel-caption">
+				      	Desert in America
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="<?php echo base_url();?>assets/img/Hydrangeas.jpg" alt="...">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="<?php echo base_url();?>assets/img/Jellyfish.jpg" alt="...">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				  </div>
+				
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left"></span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right"></span>
+				  </a>
+			</div>
+			
+			<div class="jumbotron">
+	        	<h1>Jumbotron heading</h1>
+	        	<p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	        	<p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+	      	</div>
+	      	
+	      	
+	      	<div class="row marketing">
+		        <div class="col-lg-6">
+		          <h4>Subheading</h4>
+		          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+		
+		          <h4>Subheading</h4>
+		          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+		
+		          <h4>Subheading</h4>
+		          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+		        </div>
+		
+		        <div class="col-lg-6">
+		          <h4>Subheading</h4>
+		          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+		
+		          <h4>Subheading</h4>
+		          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+		
+		          <h4>Subheading</h4>
+		          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+		        </div>
+		      </div>
+		
+		      <div class="footer">
+		        <p>&copy; Company 2014</p>
+		      </div>
+		      
+		</div>	<!-- /container -->
+		
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	    
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    	<script src="<?php echo base_url();?>assets/js/docs.min.js"></script>
+    		    
+	</body>
 </html>
